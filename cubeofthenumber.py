bc346ff74016007cd0ef7802e9c17c331827cafd
sakthivel-dev <sakthivel272000@gmail.com>
2022-07-26 10:42:18 +0530

    cube of number

---
+++ cubeofthenumber.py
@@ -0,0 +1,15 @@
+from re import I
+
+
+n=int(input())
+total=0
+for i in range(1,n+1):
+    sum=i**3
+    print(sum)
+    total=total+sum
+print("the total number is",total)
+   
+
+
+
+
--- sumoftheseries1.py
+++ sumoftheseries1.py
@@ -1,5 +1,4 @@
 n=int(input())
-sum =0.000000
 for i in range(1,n+1):
     a = float((1/i)**i)
     sum = sum + a
